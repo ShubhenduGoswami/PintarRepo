a753cba3048a4a3e6fd080255291ea69459bd35b
Shubhendu Goswami <shubhendugoswami@hotmail.com>
2018-04-06 02:55:45 +0530

    Client-side Validation completed for all the registration fields. Registration and checking availability of username will be done tomorrow.

--- Repor/Pintar/WebContent/resources/js/controller/ValidationController.js
+++ Repor/Pintar/WebContent/resources/js/controller/ValidationController.js
@@ -4,17 +4,34 @@
 
 app.controller("ValidationController", function($scope, $http, ValidationService) {
 	
+	$scope.nameValidated = false;
+	$scope.userNameValidated = false;
+	$scope.phoneValidated = false;
+	$scope.emailValidated = false;
+	$scope.passwordValidated = false;
+	$scope.rPasswordValidated = false;
+	
 	$scope.validateName = function(name) {
-		ValidationService.validateName(name);
+		$scope.nameValidated = !ValidationService.validateName(name);
 	};
 	
-	$scope.validateUserName = function() {
-		ValidationService.validateuserName($scope.userName);
+	$scope.validateUserName = function(username) {
+		$scope.userNameValidated = !ValidationService.validateUserName(username);
 	};
 	
-	$scope.validatePhone = function() {
-		ValidationService.validatePhone($scope.phone)
-	}
+	$scope.validatePhone = function(phone) {
+		$scope.phoneValidated = !ValidationService.validatePhone(phone);
+	};
 	
+	$scope.validateEmail = function(email) {
+		$scope.emailValidated = !ValidationService.validateEmail(email);
+	};
 	
+	$scope.validatePassword = function(password) {
+		$scope.passwordValidated = !ValidationService.validatePassword(password);
+	};
+	
+	$scope.validaterPassword = function(rPassword, password) {
+		$scope.rPasswordValidated = !ValidationService.validaterPassword(rPassword, password);
+	};
 });
--- Repor/Pintar/WebContent/resources/js/service/ValidationService.js
+++ Repor/Pintar/WebContent/resources/js/service/ValidationService.js
@@ -5,27 +5,32 @@
 app.service("ValidationService",function() {
 	
 	this.validateName = function(name) {
-		alert(name);
+		var regex = /^[A-Za-z ]{5,50}$/;
+		return regex.test(name);
 	};
 	
 	this.validateUserName = function(username) {
-		
+		var regex = /^[A-Za-z0-9_$-&]{6,30}$/;
+		return regex.test(username);
 	};
 	
 	this.validatePhone = function(phone) {
-		
+		var regex = /^[987]{1}[0-9]{9}$/;
+		return regex.test(phone);
 	};
 	
-	this.emailAddress = function(email) {
-		
+	this.validateEmail = function(email) {
+		var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return regex.test(email);
 	};
 	
-	this.password = function(password) {
-		
+	this.validatePassword = function(password) {
+		var regex = /^([a-zA-Z0-9@*#]{8,15})$/;
+		return regex.test(password);
 	};
 	
-	this.repeatPassword = function(rPassword) {
-		
+	this.validaterPassword = function(rPassword, password) {
+		return (rPassword==password);
 	};
 	
 });
