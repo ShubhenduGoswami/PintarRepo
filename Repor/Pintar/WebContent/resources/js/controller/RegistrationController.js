31903cb0271f6063ea21c4c217ff212a3edfa24c
Shubhendu Goswami <shubhendugoswami@hotmail.com>
2018-04-07 18:22:09 +0530

    Changes - April 7, 2018
     -> Added registration functionality.
     -> Removed Validation Controller. Calling Validation Service from
    RegistrationController now.
     -> Receiving success response from Salesforce RESTful WebService.

--- Repor/Pintar/WebContent/resources/js/controller/RegistrationController.js
+++ Repor/Pintar/WebContent/resources/js/controller/RegistrationController.js
@@ -2,11 +2,49 @@
  * 
  */
 
-app.controller("RegistrationController", function($scope, RegistrationService) {
+app.controller("RegistrationController", function($scope, RegistrationService, ValidationService) {
 	
-	$scope.register = function(details){
-		alert(JSON.stringify(details));
-		//RegistrationService.register();
+	$scope.validation = {};
+	
+	$scope.validation.nameValidated = false;
+	$scope.validation.userNameValidated = false;
+	$scope.validation.phoneValidated = false;
+	$scope.validation.emailValidated = false;
+	$scope.validation.passwordValidated = false;
+	$scope.validation.rPasswordValidated = false;
+	
+	$scope.validateName = function(name) {
+		$scope.validation.nameValidated = !ValidationService.validateName(name);
+	};
+	
+	$scope.validateUserName = function(username) {
+		$scope.validation.userNameValidated = !ValidationService.validateUserName(username);
+	};
+	
+	$scope.validatePhone = function(phone) {
+		$scope.validation.phoneValidated = !ValidationService.validatePhone(phone);
+	};
+	
+	$scope.validateEmail = function(email) {
+		$scope.validation.emailValidated = !ValidationService.validateEmail(email);
+	};
+	
+	$scope.validatePassword = function(password) {
+		$scope.validation.passwordValidated = !ValidationService.validatePassword(password);
+	};
+	
+	$scope.validaterPassword = function(rPassword, password) {
+		$scope.validation.rPasswordValidated = !ValidationService.validaterPassword(rPassword, password);
+	};
+	
+	
+	$scope.register = function(){
+		
+		alert(JSON.stringify($scope.userDetails));
+		
+		/*delete $scope.userDetails.rPassword;
+		$scope.userDetails.userType="Painter";
+		RegistrationService.register($scope.userDetails);*/
 	}
 	
 });
--- Repor/Pintar/WebContent/resources/js/service/RegistrationService.js
+++ Repor/Pintar/WebContent/resources/js/service/RegistrationService.js
@@ -2,10 +2,17 @@
  * 
  */
 
-app.service("RegistrationService", function() {
+app.service("RegistrationService", function($http) {
 	
-	this.register = function() {
-		alert("Register");
+	this.registerServiceURL = "https://letuspaint--developer--edition-ap5-force-com-u8z5cgpq3zn6.runscope.net/services/apexrest/Register/";
+	this.register = function(userData) {
+		
+		$http.post(this.registerServiceURL,userData)
+			.then(function(response) {
+				console.log(response.data);
+			}, function(responseError) {
+				console.log(responseError.data);
+			});
 	};
 	
 });
